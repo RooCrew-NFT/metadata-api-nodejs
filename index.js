10b388d8d53802f0affab4ba7b6164011d435180
markt <m4rk.tttt@gmail.com>
2021-09-09 17:10:14 -0400

    updated RooCrew metadata API

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const RooCrew = require('./src/RooCrewABI.json')
 
 const PORT = process.env.PORT || 5000
 
-const address = "0xcB9A0de0dA98Be54dcfbB28A9D739b649B19C135";
+const address = "0x10e3Ea99a188a0EC73093127b5FBFeD852dCd657";
 
-const web3 = new Web3("https://eth-rinkeby.alchemyapi.io/v2/su5-8Reiyu3oQS6590yr3o-oy6FWNnha");
+const web3 = new Web3("https://eth-mainnet.alchemyapi.io/v2/JfPp8Y_Yi8zvS1fVLWSe6wUW2LMn9sqG");
 
 const instance = new web3.eth.Contract(
 	RooCrew.abi,
@@ -38,39 +38,26 @@ app.get('/', function(req, res) {
 app.get('/api/token/:token_id', function(req, res) {
   async function doIt() {
 	  supply = await getSupply();
-	  if (parseInt(req.params.token_id) <= supply) {
-		  const tokenId = parseInt(req.params.token_id).toString()
-		  const person = db[tokenId]
-		  const bdayParts = person.birthday.split(' ')
-		  const day = parseInt(bdayParts[1])
-		  const month = parseInt(bdayParts[0])
+	  const token_id = parseInt(req.params.token_id);
+	  const db_len = Object.keys(db).length;
+	  if ((token_id <= supply) && (token_id <= db_len)) {
+		  const token_str = token_id.toString()
+		  const roo = db[token_str]
 		  const data = {
-			'name': person.name,
-			'attributes': {
-			  'birthday': person.birthday,
-			  'birth month': monthName(month),
-			  'zodiac sign': zodiac(day, month),
-			  // 'age': moment().diff(person.birthday, 'years')
-			},
-			'image': `${HOST}/images/${tokenId}.png`
+			'name': roo.name,
+			'attributes': roo.attributes,
+			'description': roo.description,
+			'image': roo.image
 		  }
 		res.send(data)
 	  } else {
-		const tokenId = parseInt(req.params.token_id).toString()
-		const person = db_fake[1]
-		const bdayParts = person.birthday.split(' ')
-		const day = parseInt(bdayParts[1])
-		const month = parseInt(bdayParts[0])
-		const data = {
-			'name': person.name,
-			'attributes': {
-			  'birthday': person.birthday,
-			  'birth month': monthName(month),
-			  'zodiac sign': zodiac(day, month),
-			  // 'age': moment().diff(person.birthday, 'years')
-			},
-			'image': `https://ipfs.io/ipfs/QmdbkLGih7EzRUnnMBFUgjoUWFibCKcJrj4ZAbJ8vjrfDR/image.png`
-		}
+		  const token_str = "1";
+		  const roo = db_fake[token_str]
+		  const data = {
+			'name': '#' + token_id.toString() + ' ' + roo.name,
+			'description': roo.description,
+			'image': roo.image
+		  }
 		res.send(data)
 	  }
   }
@@ -80,17 +67,4 @@ app.get('/api/token/:token_id', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
 
--- src/database-fake.js
+++ src/database-fake.js
@@ -1,6 +1,9 @@
 module.exports = {
   "1": {
-    "name": "unknown",
-    "birthday": "10-18"
-  }}
+    "name": "Unknown Roo",
+    "description": "Roo Crew is a unique collection of 5,000 handcrafted NFTs. These boxing, soccer, football, baseball, and basketball kangaroos are bringing their talents to the Ethereum blockchain.",
+    "image": "https://gateway.pinata.cloud/ipfs/QmVey8UjUj8oGtGkoweupTqjj2qkdq9rgzibS21LthMmFM"
+  }
+}
+
 
